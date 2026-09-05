a06957d34b84f622dba90c0bf0d0ef0780e7ac5b
tugce28926-rgb <tugce28926@gmail.com>
2026-09-05 12:05:38 +0300

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,13 +8,19 @@ import '../models/baglanti_model.dart';
 import '../models/fatura_irsaliye_model.dart';
 
 class ApiService {
-  final Dio _dio = Dio(BaseOptions(
-    baseUrl: AppConstants.baseUrl,
-    connectTimeout: const Duration(seconds: 10),
-    receiveTimeout: const Duration(seconds: 60),
-  ));
-
+  late final Dio _dio;
   String activeCompany = AppConstants.defaultCompany;
+
+  ApiService() {
+    _dio = Dio(BaseOptions(
+      baseUrl: 'https://eadonline.site/api/v1/',
+      connectTimeout: const Duration(seconds: 15),
+      receiveTimeout: const Duration(seconds: 60),
+      headers: {
+        'Accept': 'application/json',
+      },
+    ));
+  }
 
   void setCompany(String companyDb) {
     activeCompany = companyDb;
@@ -23,32 +29,52 @@ class ApiService {
 
   Future<KpiData> getKpiData() async {
     final res = await _dio.get('kpi', queryParameters: {'company': activeCompany});
-    return KpiData.fromJson(res.data);
+    if (res.data is Map<String, dynamic>) {
+      return KpiData.fromJson(res.data);
+    } else if (res.data is Map) {
+      return KpiData.fromJson(Map<String, dynamic>.from(res.data));
+    }
+    throw Exception('Geçersiz KPI yanıtı: ${res.data}');
   }
 
   Future<List<RecentInvoice>> getRecentInvoices() async {
     final res = await _dio.get('invoices/recent', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => RecentInvoice.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => RecentInvoice.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<RecentWaybill>> getRecentWaybills() async {
     final res = await _dio.get('waybills/recent', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => RecentWaybill.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => RecentWaybill.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<BankAccount>> getBanks() async {
     final res = await _dio.get('banks', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => BankAccount.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => BankAccount.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<BankTransaction>> getBankTransactions(String bankName) async {
     final res = await _dio.get('banks/${Uri.encodeComponent(bankName)}/transactions', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => BankTransaction.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => BankTransaction.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<Map<String, dynamic>>> getGiders() async {
     final res = await _dio.get('gider', queryParameters: {'company': activeCompany});
-    return List<Map<String, dynamic>>.from(res.data);
+    if (res.data is List) {
+      return (res.data as List).map((e) => Map<String, dynamic>.from(e)).toList();
+    }
+    return [];
   }
 
   Future<bool> createBankTransaction({
@@ -74,37 +100,49 @@ class ApiService {
       'expenseItem': expenseItem,
       'eftFee': eftFee,
     });
-    return res.data['ok'] == true;
+    return res.data != null && res.data['ok'] == true;
   }
 
   Future<List<CariSummary>> getDebtors() async {
     final res = await _dio.get('cariler/borclular', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => CariSummary.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<CariSummary>> getCreditors() async {
     final res = await _dio.get('cariler/alacaklilar', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => CariSummary.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<CariSummary>> getAllCaris() async {
     final res = await _dio.get('cariler/tum', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => CariSummary.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<List<Baglanti>> getBaglantilar() async {
     final res = await _dio.get('baglantilar', queryParameters: {'company': activeCompany});
-    return (res.data as List).map((e) => Baglanti.fromJson(e)).toList();
+    if (res.data is List) {
+      return (res.data as List).map((e) => Baglanti.fromJson(Map<String, dynamic>.from(e))).toList();
+    }
+    return [];
   }
 
   Future<bool> hideBank(String bankName) async {
     final res = await _dio.post('banks/${Uri.encodeComponent(bankName)}/hide', data: {'company': activeCompany});
-    return res.data['ok'] == true;
+    return res.data != null && res.data['ok'] == true;
   }
 
   Future<bool> unhideBank(String bankName) async {
     final res = await _dio.post('banks/${Uri.encodeComponent(bankName)}/unhide', data: {'company': activeCompany});
-    return res.data['ok'] == true;
+    return res.data != null && res.data['ok'] == true;
   }
 
   Future<Map<String, dynamic>> getFaturaDetail(String evrakno) async {
@@ -122,7 +160,10 @@ class ApiService {
     final res = await _dio.get(
       '$endpoint${Uri.encodeComponent(evrakno)}',
       queryParameters: {'company': activeCompany},
-      options: Options(responseType: ResponseType.bytes, receiveTimeout: const Duration(seconds: 60)),
+      options: Options(
+        responseType: ResponseType.bytes,
+        receiveTimeout: const Duration(seconds: 60),
+      ),
     );
     return Uint8List.fromList(res.data);
   }
