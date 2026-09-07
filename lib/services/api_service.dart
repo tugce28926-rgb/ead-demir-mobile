349b618b210480f79f6eab593b7bf20600838c9f
tugce28926-rgb <tugce28926@gmail.com>
2026-09-07 13:42:33 +0300

    Update api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,6 +8,23 @@ import '../models/baglanti_model.dart';
 import '../models/fatura_irsaliye_model.dart';
 
 class ApiService {
+  static String currentUser = 'TUĞÇE';
+  static bool isAdmin = false;
+  static List<String> allowedOperations = [];
+  static bool get isLoggedIn => currentUser.isNotEmpty;
+
+  static void setUser(String username, {bool admin = false, List<String>? ops}) {
+    currentUser = username;
+    isAdmin = admin;
+    allowedOperations = ops ?? [];
+  }
+
+  static void logout() {
+    currentUser = '';
+    isAdmin = false;
+    allowedOperations = [];
+  }
+
   late final Dio _dio;
   String activeCompany = AppConstants.defaultCompany;
 
@@ -18,6 +35,7 @@ class ApiService {
       receiveTimeout: const Duration(seconds: 60),
       headers: {
         'Accept': 'application/json',
+        'X-User-Name': currentUser,
       },
     ));
   }
@@ -27,7 +45,35 @@ class ApiService {
     _dio.options.headers['X-Company-Db'] = companyDb;
   }
 
+  void syncUserHeader() {
+    _dio.options.headers['X-User-Name'] = currentUser;
+  }
+
+  Future<Map<String, dynamic>> login(String username, String password) async {
+    try {
+      final res = await _dio.post('auth/login', data: {
+        'username': username,
+        'password': password,
+      });
+      if (res.data is Map && res.data['ok'] == true) {
+        final uName = res.data['username'] ?? username;
+        final isAdm = res.data['isAdmin'] == true;
+        final ops = (res.data['allowedOperations'] as List?)?.map((e) => e.toString()).toList() ?? [];
+        setUser(uName.toString(), admin: isAdm, ops: ops);
+        syncUserHeader();
+        return {'ok': true, 'username': uName, 'isAdmin': isAdm};
+      }
+      return {'ok': false, 'error': res.data['error'] ?? 'Giriş başarısız'};
+    } on DioException catch (e) {
+      final err = e.response?.data?['error'] ?? e.message ?? 'Bağlantı hatası';
+      return {'ok': false, 'error': err.toString()};
+    } catch (e) {
+      return {'ok': false, 'error': e.toString()};
+    }
+  }
+
   Future<KpiData> getKpiData() async {
+    syncUserHeader();
     final res = await _dio.get('kpi', queryParameters: {'company': activeCompany});
     if (res.data is Map<String, dynamic>) {
       return KpiData.fromJson(res.data);
@@ -38,6 +84,7 @@ class ApiService {
   }
 
   Future<List<RecentInvoice>> getRecentInvoices() async {
+    syncUserHeader();
     final res = await _dio.get('invoices/recent', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => RecentInvoice.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -46,6 +93,7 @@ class ApiService {
   }
 
   Future<List<RecentWaybill>> getRecentWaybills() async {
+    syncUserHeader();
     final res = await _dio.get('waybills/recent', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => RecentWaybill.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -54,6 +102,7 @@ class ApiService {
   }
 
   Future<List<BankAccount>> getBanks() async {
+    syncUserHeader();
     final res = await _dio.get('banks', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => BankAccount.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -62,6 +111,7 @@ class ApiService {
   }
 
   Future<List<BankTransaction>> getBankTransactions(String bankName) async {
+    syncUserHeader();
     final res = await _dio.get('banks/${Uri.encodeComponent(bankName)}/transactions', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => BankTransaction.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -70,6 +120,7 @@ class ApiService {
   }
 
   Future<List<Map<String, dynamic>>> getGiders() async {
+    syncUserHeader();
     final res = await _dio.get('gider', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => Map<String, dynamic>.from(e)).toList();
@@ -87,7 +138,9 @@ class ApiService {
     String? description,
     String? expenseItem,
     double? eftFee,
+    String? username,
   }) async {
+    syncUserHeader();
     final res = await _dio.post('banks/transaction', data: {
       'company': activeCompany,
       'operationType': operationType,
@@ -99,11 +152,14 @@ class ApiService {
       'description': description,
       'expenseItem': expenseItem,
       'eftFee': eftFee,
+      'username': username ?? currentUser,
+      'kullanici': username ?? currentUser,
     });
     return res.data != null && res.data['ok'] == true;
   }
 
   Future<List<CariSummary>> getDebtors() async {
+    syncUserHeader();
     final res = await _dio.get('cariler/borclular', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -112,6 +168,7 @@ class ApiService {
   }
 
   Future<List<CariSummary>> getCreditors() async {
+    syncUserHeader();
     final res = await _dio.get('cariler/alacaklilar', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -120,6 +177,7 @@ class ApiService {
   }
 
   Future<List<CariSummary>> getAllCaris() async {
+    syncUserHeader();
     final res = await _dio.get('cariler/tum', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => CariSummary.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -128,6 +186,7 @@ class ApiService {
   }
 
   Future<List<Baglanti>> getBaglantilar() async {
+    syncUserHeader();
     final res = await _dio.get('baglantilar', queryParameters: {'company': activeCompany});
     if (res.data is List) {
       return (res.data as List).map((e) => Baglanti.fromJson(Map<String, dynamic>.from(e))).toList();
@@ -136,26 +195,31 @@ class ApiService {
   }
 
   Future<bool> hideBank(String bankName) async {
+    syncUserHeader();
     final res = await _dio.post('banks/${Uri.encodeComponent(bankName)}/hide', data: {'company': activeCompany});
     return res.data != null && res.data['ok'] == true;
   }
 
   Future<bool> unhideBank(String bankName) async {
+    syncUserHeader();
     final res = await _dio.post('banks/${Uri.encodeComponent(bankName)}/unhide', data: {'company': activeCompany});
     return res.data != null && res.data['ok'] == true;
   }
 
   Future<Map<String, dynamic>> getFaturaDetail(String evrakno) async {
+    syncUserHeader();
     final res = await _dio.get('fatura/${Uri.encodeComponent(evrakno)}', queryParameters: {'company': activeCompany});
     return Map<String, dynamic>.from(res.data);
   }
 
   Future<Map<String, dynamic>> getIrsaliyeDetail(String evrakno) async {
+    syncUserHeader();
     final res = await _dio.get('irsaliye/${Uri.encodeComponent(evrakno)}', queryParameters: {'company': activeCompany});
     return Map<String, dynamic>.from(res.data);
   }
 
   Future<Uint8List> getDocumentPdf(String type, String evrakno) async {
+    syncUserHeader();
     final endpoint = type == 'fatura' ? 'fatura/pdf/' : 'irsaliye/pdf/';
     final res = await _dio.get(
       '$endpoint${Uri.encodeComponent(evrakno)}',
@@ -175,7 +239,9 @@ class ApiService {
     required double amount,
     String? description,
     DateTime? date,
+    String? username,
   }) async {
+    syncUserHeader();
     final res = await _dio.post('pos-tahsilat', data: {
       'company': activeCompany,
       'bankName': bankName,
@@ -184,11 +250,14 @@ class ApiService {
       'amount': amount,
       'description': description,
       'date': date?.toIso8601String(),
+      'username': username ?? currentUser,
+      'kullanici': username ?? currentUser,
     });
     return res.data != null && res.data['ok'] == true;
   }
 
   Future<bool> deleteBankTransaction(dynamic ref) async {
+    syncUserHeader();
     final res = await _dio.post('banks/transactions/$ref/delete', data: {
       'company': activeCompany,
     });
